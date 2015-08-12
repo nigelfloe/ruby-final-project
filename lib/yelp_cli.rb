606c200dc81f7389bfaf93a37f381f104e72e8f7
nigelfloe <dwr229@gmail.com>
2015-08-12 15:32:42 -0400

    v3

--- lib/yelp_cli.rb
+++ lib/yelp_cli.rb
@@ -2,35 +2,30 @@ require_relative '../config/environment'
 require 'pry'
 
 class CommandLine
-  attr_accessor :location, :food_type, :sort, :counter, :params, :response
+  attr_accessor :location, :term, :sort, :counter, :params, :response
 
   def initialize
     @yelp = YelpApi.new
-    @food_type = nil
+    @term = nil
     @counter = 0
     @offset = 0
     @sort = 0
-  end
-
-  def params
-    @params ={term: "#{@food_type}", limit: 5, offset: @offset, category_filter: 'restaurants', sort: @sort}
   end
 
   def run_command
     puts
     puts Rainbow("Welcome to the Yelp CLI!").red.bright
     puts
-    puts "This application lets you do a simple Yelp restaurant search from your command line."
+    puts "This application lets you do a simple Yelp search from your command line."
     help
     @command=nil
     while @command != 'exit'
       @command = gets.downcase.strip
       if @command == 'search'
         location
-        food_type
+        term
         sort
-        params
-        restaurant_list
+        result_list
         help
       elsif @command == "help"
         help
@@ -46,7 +41,7 @@ class CommandLine
 
   def help
     puts
-    puts "Input " + Rainbow("'search'").bright.blue + " to seach for restaurants in your area."
+    puts "Input " + Rainbow("'search'").bright.blue + " to seach for businesses in your area."
     puts "Type " + Rainbow("'exit'").bright.red + " at any time to stop the program"
     puts "or type " + Rainbow("'help'").bright.green + " to view this message again " + Rainbow(":)").bright.magenta
     puts
@@ -54,16 +49,16 @@ class CommandLine
 
   def location
     puts
-    puts "Enter a location"
+    puts "Where are you? (Zip code or address is best!)"
     input = gets.strip
     input.downcase == "exit" ? exit : @location = input
   end
 
-  def food_type
+  def term
     puts
-    puts "Enter a food type"
+    puts "What are you trying to find? (i.e.: 'restaurants', 'plumber', etc.)"
     input = gets.strip
-    input.downcase == "exit" ? exit : @food_type = input
+    input.downcase == "exit" ? exit : @term = input
   end
 
   def sort
@@ -82,42 +77,53 @@ class CommandLine
     @sort = input.to_i - 1
   end
 
-  def restaurant_list
-      begin
-        @response = @yelp.client.search(@location, @params)
-        # binding.pry
-      rescue Yelp::Error::UnavailableForLocation
-        puts "Yelp has no data for that location"
-      end
+  def set_params(offset)
+    @params ={term: "#{@term}", limit: 5, offset: offset, sort: @sort}
+  end
+
+  def yelp_search
+    begin
+      response = @yelp.client.search(@location, @params)
+    rescue Yelp::Error::UnavailableForLocation
+      puts "Yelp has no data for that location"
+    end
+    if response== nil || response.total ==0
+      puts "No Results Found"
+    else
+      response
+    end
+  end
+
+  def result_list
+    set_params(0)
+    response = yelp_search
     input = nil
-    while input != 'exit' && @offset < @response.total
-      if @response
-        if @response.businesses.empty?
-          puts "No Results found"
-        else
-          @response.businesses.each.with_index(1) do |business, index|
-            # binding.pry
-            puts
-            puts "\s\s\s\s\s#{index + @offset}. #{business.name}"
-            puts "\tRating: #{business.rating}"
-            business.location.display_address.each do |line|
-              puts "\t#{line}"
-            end
-            begin
-              puts "\t#{business.phone}"
-            rescue NoMethodError
-              puts "\tno phone number listed"
-            end
+    counter = 0
+    offset = 0
+    if response
+      while input != 'exit' && offset < response.total
+        response.businesses.each.with_index(1) do |business, index|
+          puts
+          puts "\s\s\s\s\s#{index + offset}. #{business.name}"
+          puts "\tRating: #{business.rating}"
+          business.location.display_address.each do |line|
+            puts "\t#{line}"
           end
-          puts "Showing #{5 * (counter) + 1} to #{[5 * (counter + 1), @response.total].min} of #{@response.total} Products."
-          puts "Type 'next' to view the next page"
-          input = gets.strip
-          if input == "next"
-            @counter += 1
-            @offset += 5
-            params
-            @response = @yelp.client.search(@location, @params)
+          begin
+            puts "\t#{business.phone}"
+          rescue NoMethodError
+            puts "\tno phone number listed"
           end
+        end
+        puts
+        puts "Showing #{5 * (counter) + 1} to #{[5 * (counter + 1), response.total].min} of #{response.total} Products."
+        puts "Type 'next' to view the next page or 'exit' to return to the main menu"
+        input = gets.strip
+        if input == "next"
+          counter += 1
+          offset = counter * 5
+          set_params(offset)
+          response = yelp_search
         end
       end
     end
